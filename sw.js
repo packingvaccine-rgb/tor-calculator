da5a6318990e46cb34f83a87fa84708eb489ea1c
packingvaccine-rgb <packingvaccine@gmail.com>
2026-04-13 13:35:15 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,25 +1,10 @@
-const CACHE_NAME = 'tor-calc-v1';
-const assetsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon.png'
-];
+const CACHE_NAME = 'tor-calc-v2';
+const assets = ['./', './index.html', './manifest.json', './icon.png'];
 
-// Install the service worker and cache the files
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(assetsToCache);
-    })
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets)));
 });
 
-// Serve cached files when offline
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
