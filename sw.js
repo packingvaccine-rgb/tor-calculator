9768c09a801a92b02f8716de4796a69d461196d5
packingvaccine-rgb <packingvaccine@gmail.com>
2026-04-21 17:01:45 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,37 @@
-const CACHE_NAME = 'tor-calc-v4';
-const assets = ['./', './index.html', './manifest.json', './icon.png'];
+const CACHE_NAME = 'tor-cal-v4';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets)));
+// Install: Cache new assets
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting(); // Forces the waiting service worker to become active
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+// Activate: Clean up old caches (v1, v2, etc.)
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim(); // Takes control of the page immediately
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
