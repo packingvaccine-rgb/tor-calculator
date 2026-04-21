8de1af6fd79565202a4328c7977d41a37acc5a10
packingvaccine-rgb <packingvaccine@gmail.com>
2026-04-21 16:39:45 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tor-calc-v3';
+const CACHE_NAME = 'tor-calc-v4';
 const assets = ['./', './index.html', './manifest.json', './icon.png'];
 
 self.addEventListener('install', e => {
